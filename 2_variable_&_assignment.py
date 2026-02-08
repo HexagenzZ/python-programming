fa3700937a1badde0cd09e3a84d2cdc44fb194ce
HexagenzZ <mzchevalier5@gmail.com>
2026-02-09 01:40:03 +0700

    Add some Theory

---
+++ 2_variable_&_assignment.py
@@ -0,0 +1,17 @@
+# variable adalah instruksi pada komputer untuk menyimpan sesuatu dalam komputer,
+# tentunya setelah memberikan tempat menyimpan, harusnya untuk mengisi suatu tersebut.
+
+# assignment = proses pemberian nilai tepat pda suatu program.
+# ruas kiri = variabel, ruas kanan = expression, nilai, variable yang usdah jelas nilainya
+
+greeting = 'Hello World, And Universe' #Assignment
+print(greeting)
+
+addition = 2+2
+result = addition - 1
+print(result)
+
+"""
+addition di atas adalah variabel yang bernilai ekspresi 2+2
+result (variabel result) di atas merupakan hasil penjumlahan addition dan kemudian dikurang 1
+"""
